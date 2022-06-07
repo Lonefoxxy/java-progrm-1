f2a49d2a4092150082f3b16887fcc62e04851de0
Lonefoxxy <96390256+Lonefoxxy@users.noreply.github.com>
2022-06-07 12:20:35 -0700

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,14 @@
+ class Employee {
+    private String name;
+    private double salary;
+    public Employee (String n, double s) {
+        name = n;
+        salary =s;
+    }
+    public String getName(){
+        return name;
+    }
+    public double getsalary() {
+        return salary;
+    }
+}
